d0a16163e056fccf2f3f95a9238dbd8e5c288762
Markus Frankenbach <m.frankenbach@physik.uni-muenchen.de>
2026-01-09 11:15:35 +0100

    Removed precompilation workload for now. Remove dependency TCIITensorConversion.

--- src/TCI4Keldysh.jl
+++ src/TCI4Keldysh.jl
@@ -84,33 +84,4 @@ include("correlators/PartialCorrelator_pointwiseTCI.jl")
 include("correlators/KeldyshCorrelators_TCI.jl")
 include("improved_estimators/improved_estimator_TCI.jl")
 
-@compile_workload begin
-    T = 1.
-    N_MF = 100
-    ω_fer = (collect(-N_MF:N_MF-1) * (2.) .+ 1.) * π * T
-    PSFpath = joinpath(TCI4Keldysh.datadir(), "SIAM_u=0.50/PSF_nz=2_conn_zavg/")
-
-    G        = TCI4Keldysh.FullCorrelator_MF(PSFpath, ["F1", "F1dag"]; T, flavor_idx=1, ωs_ext=(ω_fer,), ωconvMat=reshape([ 1; -1], (2,1)), name="SIAM 2pG");
-    precompute_all_values(G)
-    
-    Σ_calc_aIE = 1 ./ ω_fer .+ 0*im
-    ωconvMat_a = [
-        0 -1  0;
-        0  0  1;
-        -1  0 -1;
-        1  1  0;
-    ]
-    ωconvMat_K2a = [
-        sum(view(ωconvMat_a, [2,3], [1,2]), dims=1);
-        view(ωconvMat_a, [1,4], [1,2])
-    ]
-    N_K2_bos, N_K2_fer = 50, 50
-    ω_bos = (collect(-N_K2_bos:N_K2_bos) * (2.)      ) * π * T
-    ω_fer = (collect(-N_K2_fer:N_K2_fer-1) * (2.) .+ 1.) * π * T
-    ωs_ext=(ω_bos, ω_fer)
-    K2a_data = [ TCI4Keldysh.compute_K2r_symmetric_estimator("MF", PSFpath, ("Q23", "1", "3dag"), Σ_calc_aIE; T, ωs_ext, ωconvMat=ωconvMat_K2a, flavor_idx=i) for i in 1:2]
-
-    
-end
-
 end # module TCI4Keldysh
